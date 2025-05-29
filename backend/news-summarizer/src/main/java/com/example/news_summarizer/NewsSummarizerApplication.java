8363ff50653bae4d06d97360a192978e8915b6f0
imfromkoreah <xxivong@naver.com>
2025-05-29 16:42:16 +0900

    feat: DB 서버 연결 / 서버 기본 셋팅

---
+++ backend/news-summarizer/src/main/java/com/example/news_summarizer/NewsSummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.example.news_summarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsSummarizerApplication.class, args);
+	}
+
+}
---
+++ backend/news-summarizer/src/test/java/com/example/news_summarizer/NewsSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.news_summarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsSummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
