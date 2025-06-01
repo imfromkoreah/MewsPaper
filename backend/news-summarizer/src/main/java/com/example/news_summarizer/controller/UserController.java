0ef8cc383ca16fc32f25d6fa6cdf6e87dd656aa6
imfromkoreah <xxivong@naver.com>
2025-06-01 19:46:43 +0900

    feat: 일반 로그인 화면 / 충돌 수정

--- backend/news-summarizer/src/main/java/com/example/news_summarizer/controller/UserController.java
+++ backend/news-summarizer/src/main/java/com/example/news_summarizer/controller/UserController.java
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-// src/main/java/com/example/news_summarizer/controller/UserController.java
 package com.example.news_summarizer.controller;
 
 import com.example.news_summarizer.dto.NotificationRequest;
@@ -80,7 +78,7 @@ public class UserController {
 
         // Map 리스트를 NotificationRequest 리스트로 변환
         List<NotificationRequest> notificationRequests = notificationsData.stream().map(data ->
-            new NotificationRequest(data.get("notificationType"), data.get("notificationTime"))
+                new NotificationRequest(data.get("notificationType"), data.get("notificationTime"))
         ).collect(Collectors.toList());
 
         try {
@@ -102,43 +100,3 @@ public class UserController {
         }
     }
 }
-=======
-package com.example.news_summarizer.controller;
-
-import com.example.news_summarizer.dto.AnchorRequest;
-import com.example.news_summarizer.entity.User;
-import com.example.news_summarizer.repository.UserRepository;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import java.util.Optional;
-
-@RestController
-@RequestMapping("/api/users")
-public class UserController {
-
-    private final UserRepository userRepository;
-
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @PostMapping("/nickname")
-    public ResponseEntity<?> updateNickname(@RequestBody AnchorRequest request) {
-    Long userId = request.getUserId();
-    String nickname = request.getNickname();
-
-    Optional<User> optionalUser = userRepository.findById(userId);
-
-    if (optionalUser.isPresent()) {
-        User user = optionalUser.get();
-        user.setNickname(nickname);
-        userRepository.save(user);
-        System.out.println("✅ 닉네임 수정 완료: " + user.getNickname());
-        return ResponseEntity.ok("닉네임 수정 완료");
-    } else {
-        System.out.println("❌ 해당 ID의 유저가 없습니다.");
-        return ResponseEntity.status(404).body("유저를 찾을 수 없습니다.");
-    }
-    }
-    }
->>>>>>> 22ceb0d4ce7686fe9c7abef8235517902adb0450
