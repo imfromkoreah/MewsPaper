21bf88e1a36a09d117e94c6aa5b03e01b725c119
imfromkoreah <xxivong@naver.com>
2025-06-05 03:09:36 +0900

    feat: NewsPage 수정 / 페이지 액션 추가

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -8,7 +8,7 @@ export default function Layout({ children }: { children: ReactNode }) {
       <div className="w-full max-w-md border border-gray-200 rounded shadow-sm bg-white min-h-screen relative">
         
         {/* TopNav 고정 */}
-        <div className="fixed top-0 left-1/2 transform -translate-x-1/2 w-full max-w-md  border-gray-200 bg-white z-10">
+        <div className="fixed top-0 left-1/2 transform -translate-x-1/2 w-full max-w-md  border-gray-200 bg-white z-30">
           <TopNav />
         </div>
         
@@ -18,7 +18,7 @@ export default function Layout({ children }: { children: ReactNode }) {
         </main>
         
         {/* BottomNav 고정 */}
-        <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full max-w-md  border-gray-200 bg-white z-10">
+        <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-full max-w-md  border-gray-200 bg-white z-30">
           <BottomNav />
         </div>
       </div>
